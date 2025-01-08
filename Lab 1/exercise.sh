b98e95e6e94fc6ecf8b70418907f268bc1dec5d0
AbhiramHande <105887433+AbhiramHande@users.noreply.github.com>
2025-01-09 02:17:02 +0530

    Update exercise.sh

--- Lab 1/exercise.sh
+++ Lab 1/exercise.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Refer to Labsheet 1 and/or Explanation.md if you didn't understand a specific step
 
 cd ~
 mkdir -p BITS/Pilani/P_Dept/CSIS
@@ -13,8 +14,6 @@ cd ~/BITS/Goa/G_Dept
 mkdir Chem
 cd Phy
 touch f1.txt
-#You can use cat > f1.txt and then use 'Ctrl + D' to exit and write noting in the file
-#touch command is used to to create an empty file
 cd ~/BITS/Pilani/P_Dept/CSIS
 touch f1.txt
 cd ../ECE
@@ -28,7 +27,6 @@ CS F241: Microprocessor Programming and Interfacing
 CS F301: Principles of Programming Languages
 CS F342: Computer Architecture
 CS F372: Operating Systems" > f2.txt
-#You can use cat > f1.txt followed by listing the 10 names then using 'Ctrl + D' to exit and save the file.
 cat f2.txt > ~/BITS/Hyderabad/H_Dept/Mech/file4.txt
 tail -7 ~/BITS/Hyderabad/H_Dept/Mech/file4.txt
 rm -r ~/BITS/Pilani/P_Dept/ECE
