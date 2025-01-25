3393fed3aa2c7eed170e1a27bd984ca335c35fff
Abhiram H <105887433+AbhiramHande@users.noreply.github.com>
2025-01-25 17:05:37 +0530

    Add files via upload

---
+++ Lab 3/Exercises/Question 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    // Printing the letter F
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+
+    // Printing the letter C
+    printf("  ######\n");
+    printf(" ##   ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf(" ##   ##\n");
+    printf("  ######\n");
+    return 0;
+}
