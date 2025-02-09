8940774f9b696814a68cd368c0649b85aa44da0d
Abhiram H <105887433+AbhiramHande@users.noreply.github.com>
2025-02-09 19:03:12 +0530

    Add files via upload

---
+++ Lab 4/Exercises/Question 5.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+// Don't forget to compile with -lm flag (as you are using math library)
+int main(){
+    int exp;
+    double base;
+
+    printf("Enter the base number: ");
+    scanf("%lf", &base);
+    printf("Enter the exponent: ");
+    scanf("%d", &exp);
+
+    double ans = pow(base, exp);
+    printf("%lf raised to the power of %d is %lf\n", base, exp, ans);
+    return 0;
+}
