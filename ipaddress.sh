ba7636c33c8432aa3a7669636137fd0b0df5c114
Abhiram H <105887433+AbhiramHande@users.noreply.github.com>
2025-04-05 18:05:57 +0530

    Add files via upload

---
+++ ipaddress.sh
@@ -0,0 +1,14 @@
+sudo iptables -F
+sudo iptables -X
+
+sudo iptables -A OUTPUT -d 172.18.1.0/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.1.129/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.2.0/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.2.129/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.3.0/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.3.129/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.4.0/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.16.0/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.17.0/25 -j ACCEPT
+sudo iptables -A OUTPUT -d 172.18.18.0/25 -j ACCEPT
+sudo iptables -A OUTPUT -j REJCET
