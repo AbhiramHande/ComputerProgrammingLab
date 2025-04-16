e50b3f4c9df1fab266958107b9f19fa5379eeade
Abhiram H <105887433+AbhiramHande@users.noreply.github.com>
2025-04-16 22:41:47 +0530

    Add explanation for 2nd Question

---
+++ Lab 11/Exercises/Question 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE 64
+
+/*
+ * The mistake in the question is in line 2.
+ * The question doesn't allocate any memory for the character array.
+ * This will most likely lead to a dereference of a invalid mmemory.
+ * Thus, the program might throw a Segmentation Fault.
+ * Corrected code is given below:
+ */
+
+int main(int argc, char*argv[]){ 
+    char* answer = calloc(ARRAY_SIZE, sizeof(char));  
+    printf("Please type something: ");  
+    fgets(answer, sizeof(char)*ARRAY_SIZE, stdin);
+    printf("You typed %s",answer); 
+    return 0; 
+}
+
